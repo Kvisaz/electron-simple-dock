2ec80fadd4b943174df9c3585c467b3ff23eccdc
KvisazGames <kiko@kiko.kiko>
2020-04-07 21:06:44 +0700

    master - electron remote

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,1 +1,14 @@
+// 1. preload.js run in renderer process
+// 2. so it must get app through electron.remote
+
+const electron = require('electron')
+
+console.log('electron', electron);
+console.log('electron.remote', electron.remote);
+
+const app = electron.remote.app;
+console.log('app', app);
+console.log('app path', app.getPath('exe'));
+console.log('app path', app.getAppPath());
+
 window.isDev = true;
